dd91a28264f66d2bf8ab67045a42faa0b285fae6
Artem <44943333+lifeavg@users.noreply.github.com>
2023-03-23 10:41:46 +0300

    file exporters

---
+++ Task_1_Python_introduction/exporters.py
@@ -0,0 +1,18 @@
+import json
+from pathlib import Path
+from typing import Any
+
+import dicttoxml
+
+
+def to_json(data: list[dict[str, Any]]) -> str:
+    return json.dumps(data)
+
+
+def to_xml(data: list[dict[str, Any]]) -> str:
+    return dicttoxml.dicttoxml(data, return_bytes=False, attr_type=False)  # type: ignore
+
+
+def write(data: str, out_file: Path) -> None:
+    with open(out_file, "w", encoding="utf-8") as file:
+        file.write(data)
